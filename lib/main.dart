297e1670c5ddf7e6df6dc8bca3229c7d3af3ae2d
KareemTarekQuebefyn <kareem.tarek@qubefyn.com>
2025-12-01 18:17:04 +0200

    changed the favIcon

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kareem_portfolio/pages/home_page.dart';
 import 'package:sizer/sizer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
   runApp(const KareemPortfolio());
 }
 
