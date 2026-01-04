f92d4e7b4380b7f789bec580eb133cb8abdc4ce1
KareemTarekQuebefyn <kareem.tarek@qubefyn.com>
2026-01-04 19:34:12 +0200

    adding our projects

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -24,22 +24,9 @@ List<ProjectUtils> projectList = [
       title: "Protein House",
       subTitle:
           "A comprehensive food delivery and nutrition app offering personalized meal planning, expert consultations, and lifestyle tracking, Implemented Google Maps, and multiple payment methods (Cards, Google Pay, Apple Pay, Tamara, Tabby), localization and interactive chat, Built with Flutter using clean architecture and in the final stages of being deployed.",
-      androidLink: "",
+      androidLink:
+          "https://play.google.com/store/apps/details?id=com.fived.proteinhouse",
       iosLink: ""),
-  ProjectUtils(
-      image: "assets/images/qubecommerce1.png",
-      title: "QubeCommerce App",
-      subTitle:
-          "QubeCommerce is an e-commerce platform paired with a powerful admin panel, designed to optimize product management and streamline user shopping experiences.",
-      androidLink: "",
-      iosLink: ""),
-  ProjectUtils(
-      image: "assets/images/QubeAnalytics.png",
-      title: "Qube Analytics SDk",
-      subTitle:
-          "Private advanced Flutter package for comprehensive user behavior tracking from entry to exit, capturing the entire session, monitoring all interactions, and logging errors seamlessly without affecting the user experience—enhancing productivity and performance analysis efficiently.",
-      linkedInLink:
-          "https://www.linkedin.com/posts/kareem-tarek-17a9b819a_riseupaisummit-ai-innovation-activity-7293283886728437761-atmf?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC7HPe4B_6Q-eBZsCfyPi7xVyHZl836cTuw"),
   ProjectUtils(
       image: "assets/images/qube_pos.jpeg",
       title: "Qube POS & ERP System",
@@ -48,7 +35,21 @@ List<ProjectUtils> projectList = [
       linkedInLink:
           "https://www.linkedin.com/posts/qubefyn_qubepos-software-retailtech-activity-7320008596648345601-l1Ct?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC7HPe4B_6Q-eBZsCfyPi7xVyHZl836cTuw"),
   ProjectUtils(
-      image: "assets/images/PFM.png",
+      image: "assets/images/QubeAnalytics.png",
+      title: "Qube Analytics SDk",
+      subTitle:
+          "Private advanced Flutter package for comprehensive user behavior tracking from entry to exit, capturing the entire session, monitoring all interactions, and logging errors seamlessly without affecting the user experience—enhancing productivity and performance analysis efficiently.",
+      linkedInLink:
+          "https://www.linkedin.com/posts/kareem-tarek-17a9b819a_riseupaisummit-ai-innovation-activity-7293283886728437761-atmf?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC7HPe4B_6Q-eBZsCfyPi7xVyHZl836cTuw"),
+  ProjectUtils(
+      image: "assets/images/qubecommerce1.png",
+      title: "QubeCommerce App",
+      subTitle:
+          "QubeCommerce is an e-commerce platform paired with a powerful admin panel, designed to optimize product management and streamline user shopping experiences.",
+      androidLink: "",
+      iosLink: ""),
+  ProjectUtils(
+      image: "assets/images/investments.png",
       title: "Personal Finance Management (PFM)",
       subTitle:
           "Designed to empower users to monitor their stock investments, manage monthly budgets, and organize their portfolios efficiently. It integrates with Firebase for secure authentication and real-time data storage, offering a seamless experience across Android and iOS. The app combines powerful features for tracking stock purchases and sales, budgeting for income and expenses, and categorizing stocks for better portfolio management.",
